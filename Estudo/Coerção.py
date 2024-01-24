a6005ef7fb23dcfed4de6f568d9c277ed05d92f0
Tales <talesdearaujo18@gmail.com>
2024-01-23 23:33:04 -0300

    atualizando

--- Estudo/Coerção.py
+++ Estudo/Coerção.py
@@ -34,6 +34,6 @@ altura_metros = altura_cm / 100
 print( 'Nome', nome)
 print ( ' Sobrenome',sobrenome)
 print ('Idade: ', idade)
-print (' Ano de nascimento', ano_nascimento)
+print (' Ano de nascimento : ', ano_nascimento)
 print (' E maior de idade :', maior_de_idade)
 print (' Altura em metros', altura_metros,'m')
