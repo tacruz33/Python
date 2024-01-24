3012705f045d08233466bf0080953b0412453338
Tales <talesdearaujo18@gmail.com>
2024-01-23 23:32:35 -0300

    atualizando

--- Estudo/Coerção.py
+++ Estudo/Coerção.py
@@ -1,5 +1,39 @@
 ''' Conversão de inteiros'''
 
-print(int('1'), type(int('1')))
+'''print(int('1'), type(int('1')))
 print (type(float('4') + 1))
-print(bool('1'))
+print(bool(''))
+print (str(11) + 'b')
+
+nome_completo = 'Tales'
+soma = 2+3
+print (nome_completo, soma)
+
+if soma > divisão
+ '''
+ 
+'''nome = 'tales'
+idade = 16
+maior_de_idade = idade >=18
+
+print ('Nome : ', nome ,'tem ', idade, ' anos ')
+print (' E maior?', maior_de_idade)
+'''
+
+nome ='Tales'
+sobrenome = 'Araujo'
+idade = 27
+maior_de_idade= idade >=18
+
+ano_atual = int(input('Digite o ano atual : '))
+ano_nascimento =  ano_atual - idade
+
+altura_cm = float(input('Digite sua altura em cemntimetros : '))
+altura_metros = altura_cm / 100
+
+print( 'Nome', nome)
+print ( ' Sobrenome',sobrenome)
+print ('Idade: ', idade)
+print (' Ano de nascimento', ano_nascimento)
+print (' E maior de idade :', maior_de_idade)
+print (' Altura em metros', altura_metros,'m')
