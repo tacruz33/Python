c91bb8438d41be5e5381b68c58c5c95340955ddb
Tales <talesdearaujo18@gmail.com>
2024-01-28 20:33:34 -0300

    atualizando

--- Estudo/exercicios2.py
+++ Estudo/exercicios2.py
@@ -1,3 +1,15 @@
-divisão = 10 / 3
+'''divisão = 10 / 3
 
-print (divisão)
+print (divisão)'''
+
+""" Concatenação """
+as_vzes = 'A' * 10
+print (as_vzes)
+
+nome = 'Tales Araujo'
+altura = 1.80
+peso = 95
+
+imc = (peso / (altura**2))
+
+print ('Nome',nome, 'com altura de', altura, ' e IMC :', imc )
