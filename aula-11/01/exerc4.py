b3953683acac9958bb9bcd12a807734042f13ae0
Tales <talesdearaujo18@gmail.com>
2024-02-09 09:03:29 -0300

    atualização

--- aula-11/01/exerc4.py
+++ aula-11/01/exerc4.py
@@ -1,5 +1,5 @@
 
-
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média. '''
 
 nota1 = float(input('Digite a primeira nota : '))
 nota2 = float(input('Digite a segunda nota : '))
@@ -8,4 +8,4 @@ nota4 = float(input('Digite a quarta nota : '))
 
 media = ((nota1 + nota2 + nota3 + nota4) // 4 )
 
-print(' A média bimestral é : ', media)
+print(' A média bimestral é : ', media)  
