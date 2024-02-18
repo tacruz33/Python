7ee580df2a19cf51e1d898986d4d4e614652496c
Tales <talesdearaujo18@gmail.com>
2024-02-18 19:40:22 -0300

    revisando

--- Estudo/Cálculo de IMc.py
+++ Estudo/Cálculo de IMc.py
@@ -7,3 +7,5 @@ imc = peso / (altura ** 2)
 print ('Seu nome : ', nome, )
 print ('Altura =', altura,)  
 print ('IMC = ',round( imc, 2))  #função round para arredondar o resultado')
+
+print (...) # não gera erro os trẽs pontinhos
