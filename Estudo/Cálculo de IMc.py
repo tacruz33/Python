152cb49b743930e05933b6a1a50bc42ff63cf391
Tales <talesdearaujo18@gmail.com>
2024-02-18 19:38:30 -0300

    revisando

---
+++ Estudo/Cálculo de IMc.py
@@ -0,0 +1,9 @@
+nome = 'Tales'
+altura = 1.74
+peso = 95
+
+imc = peso / (altura ** 2)
+
+print ('Seu nome : ', nome, )
+print ('Altura =', altura,)  
+print ('IMC = ',round( imc, 2))  #função round para arredondar o resultado')
