b5224ee41da8dd28d43754c2306a4c1cf7620bc4
Tales <talesdearaujo18@gmail.com>
2024-03-05 23:37:25 -0300

    atualizando

--- Estudo/Aula 15.py
+++ Estudo/Aula 15.py
@@ -1,6 +1,10 @@
 #Operadores lógiicos
 
-entrada = input(' [E]ntrar [S]air ')
+# And - todas as condições precisam ser verdadeiras.
+
+
+
+'''entrada = input(' [E]ntrar [S]air ')
 senha_digitada = input('Senha : ')
 
 senha_permitida = '123456'
@@ -10,4 +14,19 @@ senha_permitida = '123456'
 if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida :
     print('Entrar')
 else: 
-    print ('Sair')
+    print ('Sair') '''
+    
+'''n = 10
+    
+if n > 1 :
+    if n >2:
+        if  n > 3:
+            print (' maior que 3')
+        else :
+            print (' menor que 3 ')
+    else :
+        print ('menor que 2')
+else :
+    print(' menor que 1')'''
+    
+print( False or True)
